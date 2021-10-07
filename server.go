7bd10e54855a5bbbad111862bbac5b9c25482e06
Alex-mumo <alexymumo2000@gmail.com>
2021-10-07 22:07:03 +0300

    Initial commit

---
+++ server.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"strings"
+)
+
+const address = "localhost:8888"
+
+func capitalized(conn net.Conn) {
+	reader := bufio.NewReader(conn)
+	data, err := reader.ReadString('\n')
+
+	if err != nil {
+		fmt.Print("error reading the file: %s\n", err.Error())
+		return
+	}
+	fmt.Print("Received data: %s\n", data)
+
+	//writin the data
+	conn.Write([]byte(strings.ToUpper(data)))
+	//closing the connection
+	conn.Close()
+}
+
+func main() {
+
+	//specifying the communicatin protocol and
+	listen, err := net.Listen("tcp", address)
+	if err != nil {
+		panic(err)
+	}
+	defer listen.Close()
+	fmt.Printf("listening on: %s\n", address)
+	for {
+		conn, err := listen.Accept()
+		if err != nil {
+			fmt.Printf("error while accepting the connection : %s\n", err.Error())
+			continue
+
+		}
+		go capitalized(conn)
+	}
+
+}
